9fd64624b03b43d0104f9f4ec698a7cd6f14accb
kent <kentkawashima@gmail.com>
2017-08-10 12:53:06 +0900

    Moves SequenceAlignment from main.go to alignment.go

--- main.go
+++ main.go
@@ -217,58 +217,6 @@ func GinsiCodonAlign(mafftCmd, fastaPath string, iterations int) (stdout string)
 	return
 }
 
-// SequenceAlignment is a slice of Sequence pointers.
-type SequenceAlignment []Sequence
-
-// UngappedCoords returns the row and column positions in the sequence alignment
-// where the character does not match the gap character.
-func (a SequenceAlignment) UngappedCoords(gapChar string) (rowCoords, colCoords []int) {
-	var currColCoords []int
-	for i, s := range a {
-		currColCoords = s.UngappedCoords(gapChar)
-		for c := 0; c < len(currColCoords); c++ {
-			rowCoords = append(rowCoords, i)
-		}
-		colCoords = append(colCoords, currColCoords...)
-	}
-	return
-}
-
-// UngappedPositionMatrix returns a matrix that counts only over characters
-// that does not match the gap character for each sequence in the alignment.
-// If a character in a sequence matches the gap character, -1 is inserted
-// instead of the ungapped count.
-func (a SequenceAlignment) UngappedPositionMatrix(gapChar string) (m [][]int) {
-	for _, s := range a {
-		m = append(m, s.UngappedPositionSlice(gapChar))
-	}
-	return
-}
-
-// ToUpper changes the case of all sequences to all uppercase letters.
-func (a SequenceAlignment) ToUpper() {
-	for _, s := range a {
-		s.ToUpper()
-	}
-}
-
-// ToLower changes the case of of all sequences to all lowercase letters.
-func (a SequenceAlignment) ToLower() {
-	for _, s := range a {
-		s.ToLower()
-	}
-}
-
-// ToFastaString returns the FASTA-formatted string of the sequence alignment.
-func (a SequenceAlignment) ToFastaString() string {
-	return SequencesToString(a)
-}
-
-// ToFasta saves the sequence alignment to a FASTA file.
-func (a SequenceAlignment) ToFasta(path string) {
-	WriteBufferToFile(path, SequencesToBuffer(a))
-}
-
 // StringToCharSequences loads a string generated from properly formatted
 // FASTA file into a SequenceAlignment struct.
 func StringToCharSequences(s string) (sequences SequenceAlignment) {
