282076c0a21ae524c0636f59d6abe240414bb638
kent <kentkawashima@gmail.com>
2019-03-11 12:57:52 +0900

    Adds comments to ExecMafft function

--- mafft.go
+++ mafft.go
@@ -17,26 +17,42 @@ func MafftError(err error) {
 // MAFFT functions for nucleotide and protein alignments
 
 // ExecMafft calls the MAFFT program with the given arguments.
+// Returns stdout as a string and nil if no errors are encountered.
+// If an error occurs, returns a nil string and the error encountered.
 func ExecMafft(mafftCmd string, args []string) (string, error) {
+	// Check if mafftCmd is in $PATH and returns its absolute path.
+	// However, if mafftCmd contains slashes, exec.LookPath assumes this is the absolute/relative path to the program.
+	// Because of this, exec.LookPath will not look in $PATH and directly try to run mafftCmd using the given path.
+	// Panics if LookPath returns an error
 	absPath, lookErr := exec.LookPath(mafftCmd)
 	if lookErr != nil {
+		// TODO: Handle this without panicking, Return error instead
 		panic(lookErr)
 	}
 
+	// Sets the number of threads MAFFT will use by getting the number of CPUs minus 1
+	// TODO: Allow the user to set this manually and only use all CPUs when set to -1
 	threads := runtime.NumCPU() - 1
 	args = append([]string{"--thread", strconv.Itoa(threads)}, args...)
 
+	// TODO: Add debug/verbose state which outputs the MAFFT call to stderr
 	// Output MAFFT call
 	// os.Stderr.WriteString(absPath + " " + strings.Join(args, " ") + "\n")
 
+	// Builds the command to execute the MAFFT program from the path, and for the given set of arguments
+	// This does not execute the program and the arguments yet.
+	// It only creates the struct containing the necessary information to execute the program.
 	cmd := exec.Command(absPath, args...)
-
+	// Calling .Output() executes the command and captures stdout, discards sterr.
 	stdout, err := cmd.Output()
+	// Check if the program returned an error
 	if err != nil {
+		// TODO: Do not exit, return error instead.
 		MafftError(err)
 		os.Exit(1)
 	}
 
+	// No errors encountered, returns stdout as a string and nil error.
 	return string(stdout), nil
 }
 
