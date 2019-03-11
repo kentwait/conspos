833aa0ce54f2389ea2055071f6de1aac8fdc15b5
kent <kentkawashima@gmail.com>
2019-03-11 17:22:09 +0900

    Puts exit statement inside the error function

--- error.go
+++ error.go
@@ -10,10 +10,12 @@ import (
 func EmptyAlnError(alnType, inputPath string) {
 	msg := fmt.Sprintf("Error: %s alignment from %s is empty.\nMAFFT may have encountered an error. Check if input sequences are valid.\n", alnType, inputPath)
 	os.Stderr.WriteString(msg)
+	os.Exit(1)
 }
 
 // MafftError writes to stderr that something has gone wrong with MAFFT.
 func MafftError(err error) {
 	msg := fmt.Sprintf("Error: MAFFT did not exit properly.\n Check if input sequences are valid.\n%s\n", err)
 	os.Stderr.WriteString(msg)
+	os.Exit(1)
 }
