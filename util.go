99b9d6ed9cb89dd439e692ebe71556834e560586
kent <kentkawashima@gmail.com>
2019-03-11 16:11:00 +0900

    Merges converter.go and util.go

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 
 	aln "github.com/kentwait/conspos/alignment"
 )
@@ -33,4 +34,14 @@ func MarkedAlignmentToBuffer(template aln.Alignment, consistentPos []bool, marke
 		buffer.WriteString(s.Sequence() + "\n")
 	}
 	return buffer
+}
+
+// FastaToCodonAlignment reads a FASTA file and converts it into a sequence
+// alignment of codon sequences.
+func FastaToCodonAlignment(path string) aln.Alignment {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return aln.StringToCodonAlignment(string(b))
 }
