a3c3991def668887b069008a6b74052184c8f320
kent <kentkawashima@gmail.com>
2019-04-01 13:26:55 +0900

    Updates test

--- position_test.go
+++ position_test.go
@@ -20,7 +20,7 @@ func TestScorePositions(t *testing.T) {
 		[]int{0, -1, 1, 2, -1, 3, 4, -1},
 		[]int{0, -1, 1, 2, -1, 3, -1, 4},
 	}
-	exp := []uint{
+	exp := []int{
 		3, 2, 2, 3, 3, 3, 1, 1,
 	}
 	res := ScorePositions(m1, m2, m3)
@@ -36,7 +36,7 @@ func TestMarkConsistent(t *testing.T) {
 	c := "C"
 	i := "I"
 	n := "N"
-	scoreSlice := []uint{
+	scoreSlice := []int{
 		3, 2, 2, 3, 3, 3, 1, 1,
 	}
 	exp := []string{
@@ -54,50 +54,13 @@ func TestMarkConsistent(t *testing.T) {
 func TestMarkOnlyConsistent(t *testing.T) {
 	c := "C"
 	n := "N"
-	scoreSlice := []uint{
+	scoreSlice := []int{
 		3, 2, 2, 3, 3, 3, 1, 1,
 	}
 	exp := []string{
 		c, n, n, c, c, c, n, n,
 	}
 	res := MarkOnlyConsistent(scoreSlice, 3, c, n)
-	// Test if scores are equal
-	for i := range res {
-		if res[i] != exp[i] {
-			t.Errorf("Expected and actual characters are not the same: %v != %v", res[i], exp[i])
-		}
-	}
-}
-
-func TestMarkConsistentCodon(t *testing.T) {
-	c := "C"
-	i := "I"
-	n := "N"
-	scoreSlice := []uint{
-		3, 2, 2, 3, 3, 3, 1, 1,
-	}
-	exp := []string{
-		c, c, c, i, i, i, i, i, i, c, c, c, c, c, c, c, c, c, n, n, n, n, n, n,
-	}
-	res := MarkConsistentCodon(scoreSlice, 3, c, i, n)
-	// Test if scores are equal
-	for i := range res {
-		if res[i] != exp[i] {
-			t.Errorf("Expected and actual characters are not the same: %v != %v", res[i], exp[i])
-		}
-	}
-}
-
-func TestMarkOnlyConsistentCodon(t *testing.T) {
-	c := "C"
-	n := "N"
-	scoreSlice := []uint{
-		3, 2, 2, 3, 3, 3, 1, 1,
-	}
-	exp := []string{
-		c, c, c, n, n, n, n, n, n, c, c, c, c, c, c, c, c, c, n, n, n, n, n, n,
-	}
-	res := MarkOnlyConsistentCodon(scoreSlice, 3, c, n)
 	// Test if scores are equal
 	for i := range res {
 		if res[i] != exp[i] {
